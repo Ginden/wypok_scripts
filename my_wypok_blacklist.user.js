ca3cc06ff4e3dc74b0cc70ea51d789dda5162e8f
Michał Wadas <michalwadas@gmail.com>
2015-05-20 17:03:47 +0200

    Added GUI and CommonWypokPlugin system. Now script works with tags too.

--- my_wypok_blacklist.user.js
+++ my_wypok_blacklist.user.js
@@ -2,9 +2,10 @@
 // @name        Czarnolistuj Mój Wykop
 // @namespace   my_wykop_blacklists
 // @include     http://www.wykop.pl/moj/*
-// @version     1.3.0
+// @include     http://www.wykop.pl/tag/*
+// @version     1.1.0
 // @grant       none
-// @downloadURL https://ginden.github.io/wypok_scripts/my_wypok_blacklist.user.js
+// @downloadURL https://ginden.github.io/wypok_scripts/blacklist.user.js
 // @license CC BY-SA 3.0
 // ==/UserScript==
 
@@ -57,21 +58,59 @@ function main() {
             var author = $('div[data-type="entry"] .author .showProfileSummary', el).text().trim();
             var text = $('div[data-type="entry"] .text', el).text();
             var hasBlackListedTag = false;
-            blackLists.tags.forEach(function (el) {
+            blackLists.tags.some(function (el) {
                 if (text.indexOf(el) !== -1) {
                     hasBlackListedTag = true;
+
                 }
             });
             if (hasBlackListedTag || blackLists.users.indexOf(author) >= 0) {
                 return true;
             }
             return false;
-        }).addClass('dnone').addClass('ginden_blacklist');
+        }).addClass('ginden_black_list');
 
     }
 
-    var blackList = getBlackList(removeEntries);
 
+
+    var $input = $('<input type="checkbox" id="black_list_toggle" name="black_list_toggle" />');
+    var $label = $('<label for="black_list_toggle" />');
+    function setSwitch() {
+        localStorage.black_list_on = this.checked;
+        $(document.body).toggleClass('black_list_on', this.checked);
+        $label.text((this.checked ? 'wyłącz' : String.fromCharCode(160)+'włącz')+' #czarnolisto');
+    }
+    $input.change(setSwitch);
+    $input.prop('checked', localStorage.black_list_on ? JSON.parse(localStorage.black_list_on) : true);
+    setSwitch.call($input[0]);
+    var style = document.createElement('style');
+    style.innerHTML = ['body.black_list_on .ginden_black_list {display: none; }',
+                       '#black_list_toggle {display: none}',
+                       '#black_list_toggle_cont {padding: 10px;}'
+    ].join('\n');
+
+
+    document.head.appendChild(style);
+    if (window.wykop && window.wykop.params && window.wykop.params.action === 'mywykop') {
+        $('.bspace ul').last().append($('<li id="black_list_toggle_cont">').append($input, $label));
+        getBlackList(removeEntries);
+    } else if (window.wykop && window.wykop.params && window.wykop.params.action === 'tag') {
+        getBlackList(removeEntries);
+        $('.fix-b-border > ul').last().append(
+            $('<li id="black_list_toggle_cont">').append($input, $label)
+        );
+    }
+
+    if (window.wykop) {
+        window.wykop.plugins = window.wykop.plugins || {};
+        window.wykop.plugins.Ginden = window.wykop.plugins.Ginden || {};
+        window.wykop.plugins.Ginden.MojWykopBlackList = {
+            setSwitch: function(state){
+                $input.prop('checked', !!state);
+            }
+        };
+    }
 
 }
 
@@ -79,4 +118,3 @@ function main() {
 var script = document.createElement("script");
 script.textContent = "(" + main.toString() + ")();";
 document.body.appendChild(script);
-
